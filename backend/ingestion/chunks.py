8aa219490a1b4b87159edb1d69250b2a25c8bfbf
unknown <egortesla1@gmail.com>
2026-09-23 20:42:55 +0300

    сделать вывод в json файле

--- backend/ingestion/chunks.py
+++ backend/ingestion/chunks.py
@@ -113,74 +113,16 @@ def create_chunks(documents):
 
     return chunks
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+output_dir = BASE_DIR / "output"
 
+output_dir.mkdir(exist_ok=True)
 
+output_file = output_dir / "chunks.json"
 
-if __name__ == "__main__":
+with open(output_file, "w", encoding="utf-8") as f:
+    json.dump(chunks, f, ensure_ascii=False, indent=4)
 
-    folder_path = r"D:\VSC_Code\Python"
+print(f"JSON сохранён: {output_file}")
 
-    # 1. Загрузка
-    print("\n[1] Загружаем документы...")
-
-    documents = load_documents(folder_path)
-
-    print(f"Найдено документов/страниц: {len(documents)}")
-
-    if not documents:
-        print("❌ Документы не найдены!")
-        exit()
-
-    # 2. Очистка
-    print("\n[2] Очищаем документы...")
-
-    clean_documents_data = clean_documents(documents)
-
-    print(f"После очистки: {len(clean_documents_data)}")
-
-    if not clean_documents_data:
-        print("❌ После очистки не осталось текста!")
-        exit()
-
-    # 3. Chunking
-    print("\n[3] Создаём chunks...")
-
-    chunks = create_chunks(clean_documents_data)
-
-    print(f"Создано chunks: {len(chunks)}")
-
-    if not chunks:
-        print("❌ Chunks не созданы!")
-        exit()
-
-    # 4. Показываем первый chunk
-    print("\n[4] Первый chunk:")
-    print("-" * 60)
-
-    print(f"document_id : {chunks[0]['document_id']}")
-    print(f"filename    : {chunks[0]['filename']}")
-    print(f"page        : {chunks[0]['page']}")
-    print(f"section     : {chunks[0]['section']}")
-    print(f"content     : {chunks[0]['content'][:500]}")
-
-    print("-" * 60)
-
-    # 5. Проверяем metadata
-    print("\n[5] Проверяем metadata...")
-
-    required_fields = [
-        "document_id",
-        "filename",
-        "page",
-        "section",
-        "content"
-    ]
-
-    for field in required_fields:
-
-        if field in chunks[0]:
-            print(f"✅ {field}")
-        else:
-            print(f"❌ {field}")
-
-print(json.dumps(chunks[:3], ensure_ascii=False, indent=4))
+print(json.dumps(chunks, ensure_ascii=False, indent=4))
